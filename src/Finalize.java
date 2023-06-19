17edacddbdbc1c53df61b2d72e6771e92a733938
Ann Gutuleac <ann.gutuleac@gmail.com>
2023-06-19 15:45:56 +0300

    Make lesson 4 homework

--- src/Finalize.java
+++ src/Finalize.java
@@ -1,2 +1,34 @@
-public class Finalize {
+/*
+Упражнение 4.2
+Демонстрация сборки мусора и метода finalize()
+*/
+class FDemo {
+    int x;
+
+    FDemo(int i) {
+        x = i;
+    }
+
+    // Вызывается при удалении объекта
+    protected void finalize() {
+        System.out.println("Финaлизaция " + x);
+    }
+
+    // Генерирует объект, который тотчас же уничтожается
+    void generator(int i) {
+        FDemo о = new FDemo(i);
+    }
+}
+class Finalize {
+    public static void main(String args[]) {
+        int count;
+        FDemo ob = new FDemo (0);
+/* Генерируется большое количество объектов.
+В какой-то момент должна начаться сборка мусора.
+Примечание: возможно, для того чтобы активизировать
+систему сборки мусора, количество генерируемых объектов
+придется увеличить. */
+        for (count = 1; count < 100000; count++)
+            ob.generator(count);
+    }
 }
